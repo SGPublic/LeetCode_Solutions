c37d30041533a3ecc210e5a59ec919c0eb24d217
sgpublic <3300633844@qq.com>
2021-11-05 16:00:42 +0800

    211105

---
+++ src/Y2021/Nov/Nov5/Nov5.java
@@ -0,0 +1,20 @@
+package Y2021.Nov.Nov5;
+
+import util.PrintUtil;
+
+public class Nov5 {
+    private static final int[] s1 = new int[] { 1, 2, 3, 4 };
+    private static final int[] s2 = new int[] { 1, 3, 5, 7 };
+    private static final int[] s3 = new int[] { 1, 5, 7, 8, 5, 3, 4, 2, 1 };
+    private static final int[] s4 = new int[] {
+            7, -2, 8, 10, 6, 18, 9, -8, -5, 18, 13, -6, -17, -1, -6, -9, 9, 9
+    };
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        PrintUtil.println(solution.longestSubsequence(s1, 1));
+        PrintUtil.println(solution.longestSubsequence(s2, 1));
+        PrintUtil.println(solution.longestSubsequence(s3, -2));
+        PrintUtil.println(solution.longestSubsequence(s4, 1));
+    }
+}
---
+++ src/Y2021/Nov/Nov5/Solution.java
@@ -0,0 +1,27 @@
+package Y2021.Nov.Nov5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int longestSubsequence(int[] arr, int difference) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer index : arr) {
+            if (difference != 0 && map.containsKey(index)
+                    && !map.containsKey(index - difference)) {
+                continue;
+            }
+            int length = map.getOrDefault(index - difference, 0);
+            map.remove(index - difference);
+            map.put(index, length + 1);
+        }
+
+        int ans = 0;
+        for (int index : map.values()) {
+            if (index > ans) {
+                ans = index;
+            }
+        }
+        return ans;
+    }
+}
