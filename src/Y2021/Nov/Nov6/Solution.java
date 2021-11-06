7328539d2bd7c559c964b2dac5909928fa85ccf2
SGPublic <020821sky>
2021-11-06 11:03:39 +0800

    211106

---
+++ src/Y2021/Nov/Nov6/Solution.java
@@ -0,0 +1,14 @@
+package Y2021.Nov.Nov6;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int ans = 0;
+        for (int num : nums){
+            ans ^= num;
+        }
+        for (int i = 0; i <= nums.length; i++){
+            ans ^= i;
+        }
+        return ans;
+    }
+}
