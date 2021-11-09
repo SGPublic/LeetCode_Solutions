d09bba5d313bfbaad6fb72987c6f12f2eb8a4096
sgpublic <3300633844@qq.com>
2021-11-10 00:55:44 +0800

    211108

---
+++ src/Y2021/Nov/Nov8/Solution.java
@@ -0,0 +1,26 @@
+package Y2021.Nov.Nov8;
+
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls = 0;
+        int cows = 0;
+
+        int size = secret.length();
+        int[] secretCount = new int[10];
+        int[] guessCount = new int[10];
+        char[] secretChats = secret.toCharArray();
+        char[] guessChats = guess.toCharArray();
+        for (int i = 0; i < size; i++) {
+            if (secretChats[i] == guessChats[i]) {
+                bulls++;
+            }
+            secretCount[secretChats[i] - '0']++;
+            guessCount[guessChats[i] - '0']++;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            cows += Math.min(guessCount[i], secretCount[i]);
+        }
+        return String.format("%dA%dB", bulls, cows - bulls);
+    }
+}
