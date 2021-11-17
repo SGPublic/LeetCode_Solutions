6af72025ad2bdcc0d62bb4c95e8a72978ec1323b
sgpublic <3300633844@qq.com>
2021-11-17 11:35:02 +0800

    211117

---
+++ src/Y2021/Nov/Nov17/Nov17.java
@@ -0,0 +1,22 @@
+package Y2021.Nov.Nov17;
+
+import util.PrintUtil;
+
+public class Nov17 {
+    private static final String[] s1 = new String[] {
+            "abcw", "baz", "foo", "bar", "xtfn", "abcdef"
+    };
+    private static final String[] s2 = new String[] {
+            "a", "ab", "abc", "d", "cd", "bcd", "abcd"
+    };
+    private static final String[] s3 = new String[] {
+            "a", "aa", "aaa", "aaaa"
+    };
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        PrintUtil.println(solution.maxProduct(s1));
+        PrintUtil.println(solution.maxProduct(s2));
+        PrintUtil.println(solution.maxProduct(s3));
+    }
+}
