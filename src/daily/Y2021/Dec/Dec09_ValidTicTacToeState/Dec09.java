54518cac2a0588149df1653333c0be0fee2e82ac
sgpublic <3300633844@qq.com>
2021-12-09 15:40:31 +0800

    211209

---
+++ src/daily/Y2021/Dec/Dec09_ValidTicTacToeState/Dec09.java
@@ -0,0 +1,12 @@
+package daily.Y2021.Dec.Dec09_ValidTicTacToeState;
+
+public class Dec09 {
+    private static final String[] s1 = { "O  ","   ","   " };
+    private static final String[] s2 = { "XOX"," X ","   " };
+    private static final String[] s3 = { "XXX","   ","OOO" };
+    private static final String[] s4 = { "XOX","O O","XOX" };
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/daily/Y2021/Dec/Dec09_ValidTicTacToeState/Solution.java
@@ -0,0 +1,10 @@
+package daily.Y2021.Dec.Dec09_ValidTicTacToeState;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+class Solution {
+    public boolean validTicTacToe(String[] board) {
+        // TODO [794. 有效的井字游戏](https://leetcode-cn.com/problems/valid-tic-tac-toe-state/)
+        throw new NotImplementedException();
+    }
+}
