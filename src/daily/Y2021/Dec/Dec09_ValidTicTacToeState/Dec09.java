eda745a82702cadfdd7066b8971455ce362568bf
sgpublic <3300633844@qq.com>
2021-12-12 13:03:31 +0800

    211209补

--- src/daily/Y2021/Dec/Dec09_ValidTicTacToeState/Dec09.java
+++ src/daily/Y2021/Dec/Dec09_ValidTicTacToeState/Dec09.java
@@ -1,12 +1,46 @@
 package daily.Y2021.Dec.Dec09_ValidTicTacToeState;
 
+import util.PrintUtil;
+
 public class Dec09 {
-    private static final String[] s1 = { "O  ","   ","   " };
-    private static final String[] s2 = { "XOX"," X ","   " };
-    private static final String[] s3 = { "XXX","   ","OOO" };
-    private static final String[] s4 = { "XOX","O O","XOX" };
+    private static final String[] s1 = {
+            "O  ",
+            "   ",
+            "   "
+    };
+    private static final String[] s2 = {
+            "XOX",
+            " X ",
+            "   "
+    };
+    private static final String[] s3 = {
+            "XXX",
+            "   ",
+            "OOO"
+    };
+    private static final String[] s4 = {
+            "XOX",
+            "O O",
+            "XOX"
+    };
+    private static final String[] s5 = {
+            "XOX",
+            "O X",
+            "X O"
+    };
+    private static final String[] s6 = {
+            "OXX",
+            "XOX",
+            "OXO"
+    };
 
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        PrintUtil.println(solution.validTicTacToe(s1));
+        PrintUtil.println(solution.validTicTacToe(s2));
+        PrintUtil.println(solution.validTicTacToe(s3));
+        PrintUtil.println(solution.validTicTacToe(s4));
+        PrintUtil.println(solution.validTicTacToe(s5));
+        PrintUtil.println(solution.validTicTacToe(s6));
     }
 }
--- src/daily/Y2021/Dec/Dec09_ValidTicTacToeState/Solution.java
+++ src/daily/Y2021/Dec/Dec09_ValidTicTacToeState/Solution.java
@@ -1,10 +1,79 @@
 package daily.Y2021.Dec.Dec09_ValidTicTacToeState;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 class Solution {
     public boolean validTicTacToe(String[] board) {
-        // TODO [794. 有效的井字游戏](https://leetcode-cn.com/problems/valid-tic-tac-toe-state/)
-        throw new NotImplementedException();
+        int x = 0, o = 0;
+        for (String line : board){
+            for (char c : line.toCharArray()) {
+                switch(c){
+                    case 'X':
+                        x++;
+                        break;
+                    case 'O':
+                        o++;
+                        break;
+                }
+            }
+        }
+        if (x - o > 1 || o > x){
+            return false;
+        }
+        if (x < 3){
+            return true;
+        }
+        boolean winX = win(board, 'X');
+        boolean winO = win(board, 'O');
+        if (!winO && !winX){
+            return true;
+        }
+        if (winO && winX){
+            return false;
+        }
+        if (winX && x - o == 1){
+            return true;
+        }
+        if (winO && x == o){
+            return true;
+        }
+        return false;
+    }
+
+    private static final int[][][] wins = {
+            {
+                    { 0, 0 }, { 1, 0 }, { 2, 0 }
+            },
+            {
+                    { 0, 1 }, { 1, 1 }, { 2, 1 }
+            },
+            {
+                    { 0, 2 }, { 1, 2 }, { 2, 2 }
+            },
+            {
+                    { 0, 0 }, { 0, 1 }, { 0, 2 }
+            },
+            {
+                    { 1, 0 }, { 1, 1 }, { 1, 2 }
+            },
+            {
+                    { 2, 0 }, { 2, 1 }, { 2, 2 }
+            },
+            {
+                    { 0, 0 }, { 1, 1 }, { 2, 2 }
+            },
+            {
+                    { 2, 0 }, { 1, 1 }, { 0, 2 }
+            }
+    };
+    private boolean win(String[] board, char c){
+        pointFor: for (int[][] winCase : wins){
+            for (int i = 0; i < 3; i++){
+                int[] point = winCase[i];
+                if (board[point[0]].charAt(point[1]) != c){
+                    continue pointFor;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
