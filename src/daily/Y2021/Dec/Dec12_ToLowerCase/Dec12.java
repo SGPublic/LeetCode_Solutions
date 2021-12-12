3a946d3d7a0863ec9d4fa84b5a1afc43cb6d44b3
sgpublic <3300633844@qq.com>
2021-12-12 13:09:43 +0800

    211212

---
+++ src/daily/Y2021/Dec/Dec12_ToLowerCase/Dec12.java
@@ -0,0 +1,12 @@
+package daily.Y2021.Dec.Dec12_ToLowerCase;
+
+import util.PrintUtil;
+
+public class Dec12 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        PrintUtil.println(solution.toLowerCase("Hello"));
+        PrintUtil.println(solution.toLowerCase("here"));
+        PrintUtil.println(solution.toLowerCase("LOVELY"));
+    }
+}
---
+++ src/daily/Y2021/Dec/Dec12_ToLowerCase/Solution.java
@@ -0,0 +1,9 @@
+package daily.Y2021.Dec.Dec12_ToLowerCase;
+
+import java.util.Locale;
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase(Locale.ROOT);
+    }
+}
