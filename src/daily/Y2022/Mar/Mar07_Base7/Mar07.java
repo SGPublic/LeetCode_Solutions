027af8ea153084171fa35123f1f3b4578f3de3fa
sgpublic <3300633844@qq.com>
2022-03-07 10:25:55 +0800

    220307

---
+++ src/daily/Y2022/Mar/Mar07_Base7/Mar07.java
@@ -0,0 +1,11 @@
+package daily.Y2022.Mar.Mar07_Base7;
+
+import util.PrintUtil;
+
+public class Mar07 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        PrintUtil.println(solution.convertToBase7(100));
+        PrintUtil.println(solution.convertToBase7(-7));
+    }
+}
---
+++ src/daily/Y2022/Mar/Mar07_Base7/Solution.java
@@ -0,0 +1,7 @@
+package daily.Y2022.Mar.Mar07_Base7;
+
+class Solution {
+    public String convertToBase7(int num) {
+        return Integer.toString(num, 7);
+    }
+}
